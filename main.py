b751ac1835c07aa09bd5459b5c75bf48bef8021b
AvaMarquette23 <817328@mystma.org>
2021-09-13 15:17:06 -0500

    Added top three movies

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Hello World!")
+print("This is  interesting")
+
+#Print first movie
+print("Ready Player One")
+
+#Print second movie
+print("The Princess Bride")
+
+#Print third movie
+print("Any of the Harry Potter movies")
