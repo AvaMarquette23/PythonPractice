9951f2f69c8dbaa72c3bd887657cd9e7697f01b0
AvaMarquette23 <817328@mystma.org>
2021-09-15 14:42:03 -0500

    Added four functions

--- main.py
+++ main.py
@@ -11,3 +11,48 @@ def topFiveMovies():
     #print fifth movie
     print("Lord Of The Rings")
 topFiveMovies()
+#definefunction
+def topThreeShows():
+    #print first show
+    print("The Big Bang Theory")
+    #print second show
+    print("How I Met Your Mother")
+    #print third show
+    print("Friends")
+topThreeShows()
+#define function
+def topFourBands():
+    #print first band
+    print("Weezer")
+    #print second band
+    print("Jukebox The Ghost")
+    #print third band
+    print("All Time Low")
+    #print fourth band
+    print ("Weathers")
+topFourBands()
+#define function
+def topSixAnimals():
+    #print first animal
+    print("Penguin")
+    #print second animal
+    print("Tortoise")
+    #print third animal
+    print("Sea Turtle")
+    #print fourth animal
+    print("Polar Bear")
+    #print fifth  animal
+    print("Pangolin")
+    #print sixth animal
+    print("Fox")
+topSixAnimals()
+#define function
+def topTwoBooks():
+    #print first book
+    print("Ready Player One")
+    #print second book
+    print("Etiquette And Espionage")
+topTwoBooks()
+
+
+
