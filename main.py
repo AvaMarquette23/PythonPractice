d2bcef1803f4065d0393045e7f5b49df837733e8
AvaMarquette23 <817328@mystma.org>
2021-09-16 15:16:58 -0500

    Made a madLib

--- main.py
+++ main.py
@@ -59,6 +59,22 @@ def myNameIs(name):
 myNameIs("Ava")
 myNameIs("E")
 
+def timesTen(num):
+    print(num*10)
+timesTen(42)
 
+def plusFive(num):
+    print(num + 5)
+plusFive(4)
 
+def dividedByTwo(num):
+    print(num/2)
+dividedByTwo(36)
 
+def addTwoNumbers(x, y):
+    print(x + y)
+addTwoNumbers(38.76,1084)
+
+def madLib(instrument, number1, dayweek, letteraz, majorminor, number2, number3, adjective1, adjective2, fraction, adjective3, nameofinstrument, adjective4, noun, familymember, nameofasong, adejctiveed, favoriteartistcomposer):
+    print("While I've  stayed at home, I've practiced the " + instrument + " for " +number1+ " hours every "+dayweek+". My favorite key signature to play and practice in is "+letteraz+" "+majorminor+". It has "+number2+" flats and "+number3+" sharps. I like this key signature because it is "+adjective1+" and "+adjective2+". I've also gotten awesome at counting time signatures. The time signature I love is "+fraction+", because it is "+adjective3+" to count. One instrument I want to learn how to play while I'm at home is "+nameofinstrument+", because it is "+adjective4+" and sounds like a "+noun+". My "+familymember+" likes it when I play "+nameofasong+", and always gives me a round of applause after my performances! When I return to school, my teacher will be "+adejctiveed+" of how great I am at playing my instrument. My teacher might even think I sound like "+favoriteartistcomposer+"!")
+madLib("flute", "45", "day", "i", "minor", "754", "234", "sharp", "unique", "4/5", "mind-blowing", "drumkit", "bossy", "clownfish", "mom", "MovesLikeJagger", "amazed", "Prince")
