2513109a15e7438c95dd860005c98ed6853a8f49
AvaMarquette23 <817328@mystma.org>
2021-09-17 14:17:04 -0500

    Made Mad Lib

--- main.py
+++ main.py
@@ -76,5 +76,16 @@ def addTwoNumbers(x, y):
 addTwoNumbers(38.76,1084)
 
 def madLib(instrument, number1, dayweek, letteraz, majorminor, number2, number3, adjective1, adjective2, fraction, adjective3, nameofinstrument, adjective4, noun, familymember, nameofasong, adejctiveed, favoriteartistcomposer):
-    print("While I've  stayed at home, I've practiced the " + instrument + " for " +number1+ " hours every "+dayweek+". My favorite key signature to play and practice in is "+letteraz+" "+majorminor+". It has "+number2+" flats and "+number3+" sharps. I like this key signature because it is "+adjective1+" and "+adjective2+". I've also gotten awesome at counting time signatures. The time signature I love is "+fraction+", because it is "+adjective3+" to count. One instrument I want to learn how to play while I'm at home is "+nameofinstrument+", because it is "+adjective4+" and sounds like a "+noun+". My "+familymember+" likes it when I play "+nameofasong+", and always gives me a round of applause after my performances! When I return to school, my teacher will be "+adejctiveed+" of how great I am at playing my instrument. My teacher might even think I sound like "+favoriteartistcomposer+"!")
-madLib("flute", "45", "day", "i", "minor", "754", "234", "sharp", "unique", "4/5", "mind-blowing", "drumkit", "bossy", "clownfish", "mom", "MovesLikeJagger", "amazed", "Prince")
+    print("While I've  stayed at home, I've practiced the " +
+    instrument + " for " +number1+ " hours every "+dayweek+". My favorite key signature to play and practice in is "
+    +letteraz+" "+majorminor+". It has "+number2+" flats and "+number3+
+    " sharps. I like this key signature because it is "+adjective1+" and "+adjective2+
+    ". I've also gotten awesome at counting time signatures. The time signature I love is "+fraction+
+    ", because it is "+adjective3+" to count. One instrument I want to learn how to play while I'm at home is "
+    +nameofinstrument+", because it is "+adjective4+" and sounds like a "+noun+". My "+familymember+
+    " likes it when I play "+nameofasong+
+    ", and always gives me a round of applause after my performances! When I return to school, my teacher will be "
+    +adejctiveed+" of how great I am at playing my instrument. My teacher might even think I sound like "
+    +favoriteartistcomposer+"!")
+madLib("flute", "45", "day", "i", "minor", "754", "234", "sharp", "unique", "4/5", "mind-blowing", "drumkit",
+ "bossy", "clownfish", "mom", "MovesLikeJagger", "amazed", "Prince")
